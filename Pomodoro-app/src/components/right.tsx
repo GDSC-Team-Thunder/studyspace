bb86c2cac3de00eb1071050e59d8816493e11a4b
Yujin Cho <yujincho2002@gmail.com>
2024-01-19 09:45:53 -0800

    old app -> new app

---
+++ Pomodoro-app/src/components/right.tsx
@@ -0,0 +1,14 @@
+
+const Timer = () => {
+    return (
+        <div className='side-container'>
+            <div className='side-bg'>
+            </div>
+            <div className='side-text'>
+                    <h1>RIGHT :)</h1>
+            </div>
+        </div>
+    );
+  };
+  
+export default Timer;
