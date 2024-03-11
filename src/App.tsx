16bc6a9090b123148560491ff281294f0a551c2a
varun22 <84626068+varun22@users.noreply.github.com>
2024-03-11 00:03:33 -0700

    Auth changes in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import Timer from './components/timer.tsx'
-import Right from './components/right.tsx'
-import List from './components/ToDoList/List.tsx';
-import LoginPage from './components/loginPage/LoginPage.tsx';
-import './css/App.css';
-import { useState } from 'react';
+import Timer from "./components/timer.tsx";
+import Right from "./components/right.tsx";
+import List from "./components/ToDoList/List.tsx";
+// import LoginPage from './components/loginPage/LoginPage.tsx';
+import "./css/App.css";
+// import { useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState<boolean>(true);
+  // const [loggedIn, setLoggedIn] = useState<boolean>(true);
 
   return (
-    <div className = "App">
-      {/* <div className="flex justify-center flex-row h-screen w-[97%]">
-          <List/>
-          <Timer />
-          <Right />
-      </div> */}
-      <LoginPage/>
+    <div className="App">
+      <div className="flex justify-center flex-row h-screen w-[97%]">
+        <List />
+        <Timer />
+        <Right />
+      </div>
+      {/* <LoginPage/> */}
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/ToDoList/List.tsx
+++ src/components/ToDoList/List.tsx
@@ -1,51 +1,80 @@
-import {useState} from "react";
+import { useState } from "react";
 import Task from "./Task";
 import IncompleteTask from "./IncompleteTask";
 import CompleteTask from "./CompleteTask";
 
 export default function List() {
-    const [todo, editTodo] = useState<Task[]>([]);
-    const [finished, editFinished] = useState<Task[]>([]);
-    const [addText, setAddText] = useState<string>("");
+  const [todo, editTodo] = useState<Task[]>([]);
+  const [finished, editFinished] = useState<Task[]>([]);
+  const [addText, setAddText] = useState<string>("");
 
-    const addTask = () => {
-        const newTask: Task = {id: Date.now(), description: addText};
-        
-        editTodo(current => [newTask, ...current]);
-        setAddText("");
-    }
+  const addTask = () => {
+    const newTask: Task = { id: Date.now(), description: addText };
 
-    const onComplete = (task: Task) => {
-        editTodo(current => current.filter(element => element.id != task.id));
-        editFinished(current => [task, ...current]);
-    }
+    editTodo((current) => [newTask, ...current]);
+    setAddText("");
+  };
 
-    const onIncomplete = (task: Task) => {
-        editTodo(current => [...current, task]);
-        editFinished(current => current.filter(element => element.id != task.id));
-    }
+  const onComplete = (task: Task) => {
+    editTodo((current) => current.filter((element) => element.id != task.id));
+    editFinished((current) => [task, ...current]);
+  };
 
-    const deleteIncomplete = (task: Task) => {
-        editTodo(current => current.filter(element => element.id != task.id));
-    }
+  const onIncomplete = (task: Task) => {
+    editTodo((current) => [...current, task]);
+    editFinished((current) =>
+      current.filter((element) => element.id != task.id)
+    );
+  };
 
-    const deleteComplete = (task: Task) => {
-        editFinished(current => current.filter(element => element.id != task.id));
-    }
+  const deleteIncomplete = (task: Task) => {
+    editTodo((current) => current.filter((element) => element.id != task.id));
+  };
 
-    return (
-        <div className="relative flex flex-col h-[85%] bg-[#FCFBF8] opacity-65 rounded-[25px] self-center w-[24%] p-1 justify-between">
-            <div className="flex flex-col">
-                <span className="ml-5 mt-3 self-start font-bold text-[17px]">ToDo List</span>
-                <ul className="flex flex-col w-full">
-                    {todo.map(element => <IncompleteTask task={element} onComplete={onComplete} onDelete={deleteIncomplete}/>)}
-                    {finished.map(element => <CompleteTask task={element} onIncomplete={onIncomplete} onDelete={deleteComplete}/>)}
-                </ul>
-            </div>
-            <label className="flex flex-row m-3">
-                <input className="flex flex-1 p-1 bg-slate-200 pl-3 rounded-[25px] mr-2 text-black placeholder-black" placeholder="Add text" value={addText} onChange={e => setAddText(e.target.value)}/>
-                <button className="flex rounded-full text-[17px] items-center" disabled={addText.length == 0} onClick={addTask}>+</button>
-            </label>
-        </div>
-    )
+  const deleteComplete = (task: Task) => {
+    editFinished((current) =>
+      current.filter((element) => element.id != task.id)
+    );
+  };
+
+  return (
+    <div className="relative flex flex-col h-[85%] bg-[#FCFBF8] opacity-65 rounded-[25px] self-center w-[24%] p-1 justify-between">
+      <div className="flex flex-col">
+        <span className="ml-5 mt-3 self-start font-bold text-[17px]">
+          to-do list
+        </span>
+        <ul className="flex flex-col w-full">
+          {todo.map((element) => (
+            <IncompleteTask
+              task={element}
+              onComplete={onComplete}
+              onDelete={deleteIncomplete}
+            />
+          ))}
+          {finished.map((element) => (
+            <CompleteTask
+              task={element}
+              onIncomplete={onIncomplete}
+              onDelete={deleteComplete}
+            />
+          ))}
+        </ul>
+      </div>
+      <label className="flex flex-row m-3">
+        <input
+          className="flex flex-1 p-1 bg-slate-200 pl-3 rounded-[25px] mr-2 text-black placeholder-black"
+          placeholder="Add text"
+          value={addText}
+          onChange={(e) => setAddText(e.target.value)}
+        />
+        <button
+          className="flex rounded-full text-[17px] items-center"
+          disabled={addText.length == 0}
+          onClick={addTask}
+        >
+          +
+        </button>
+      </label>
+    </div>
+  );
 }
