70b80163db15613361b372e7f6fff91a92b786c0
varun22 <84626068+varun22@users.noreply.github.com>
2024-04-10 17:16:42 -0700

    fixed JWT_SECRET_KEY

--- server/app.ts
+++ server/app.ts
@@ -3,9 +3,8 @@ import { Application } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { User } from "./models/users";
-import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 const PORT: number = 8000;
@@ -33,47 +32,36 @@ app.listen(PORT, async () => {
   }
 });
 
-// Define the router for CRUD request
-const router: Router = Router();
-
 app.post("/auth/register", async (req, res) => {
   try {
-    const user = req.body;
-    const { name, email, password } = user;
+    const { email, password } = req.body;
 
-    // Generate hashed password with bcrypt
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const isEmailAllReadyExist = await User.findOne({
-      email: email,
-    });
+    const isEmailAlreadyExist = await User.findOne({ email });
 
-    // ** Add a condition if the user exist we will send the response as email all ready exist
-    if (isEmailAllReadyExist) {
-      res.status(400).json({
+    if (isEmailAlreadyExist) {
+      return res.status(400).json({
         status: 400,
         message: "Email already in use",
       });
-      return;
     }
 
     const newUser = await User.create({
-      name,
       email,
-      password,
+      password: hashedPassword, // Store hashed password
     });
-    // Send the newUser as response;
+
     res.status(200).json({
-      status: 201,
+      status: 200,
       success: true,
-      message: " User created Successfully",
+      message: "User created Successfully",
       user: newUser,
     });
   } catch (error: any) {
-    // console the error to debug
     console.log(error);
-    // Send the error message to the client
+
     res.status(400).json({
       status: 400,
       message: error.message.toString(),
@@ -81,19 +69,15 @@ app.post("/auth/register", async (req, res) => {
   }
 });
 
-const JWT_SECRET_KEY = process.env.JWT_SECRET;
-
 app.post("/auth/login", async (req, res) => {
   try {
-    // ** Get The User Data From Body ;
     const user = req.body;
-    // ** destructure the information from user;
     const { email, password } = user;
-    // ** Check the (email/user) exist in database or not ;
+
     const isUserExist = await User.findOne({
       email: email,
     });
-    // ** if there is not any user we will send user not found;
+
     if (!isUserExist) {
       res.status(404).json({
         status: 404,
@@ -102,10 +86,9 @@ app.post("/auth/login", async (req, res) => {
       });
       return;
     }
-    // ** if the (user) exist in database we will check the password is valid or not ;
-    // ** compare the password in db and the password sended in the request body
+
     const isPasswordMatched = isUserExist?.password === password;
-    // ** if not matched send response that wrong password;
+
     if (!isPasswordMatched) {
       res.status(400).json({
         status: 400,
@@ -114,6 +97,9 @@ app.post("/auth/login", async (req, res) => {
       });
       return;
     }
+
+    const jwt = require("jsonwebtoken");
+    const JWT_SECRET_KEY = process.env.JWT_SECRET || console.log("ERROR");
     const token = jwt.sign(
       { _id: isUserExist?._id, email: isUserExist?.email },
       JWT_SECRET_KEY,
@@ -121,7 +107,7 @@ app.post("/auth/login", async (req, res) => {
         expiresIn: process.env.JWT_EXPIRES_IN,
       }
     );
-    // send the response
+
     res.status(200).json({
       status: 200,
       success: true,
@@ -129,7 +115,6 @@ app.post("/auth/login", async (req, res) => {
       token: token,
     });
   } catch (error: any) {
-    // Send the error message to the client
     res.status(400).json({
       status: 400,
       message: error.message.toString(),
