0985aa8c9efca6ecc250f56256285ee38d29938f
varun22 <84626068+varun22@users.noreply.github.com>
2024-04-21 21:20:11 -0700

    added middleware routes

---
+++ server/Middlewares/AuthMiddleware.ts
@@ -0,0 +1,19 @@
+const User = require("../models/users.ts");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.exports.userVerification = (req: any, res: any) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ status: false });
+  }
+  jwt.verify(token, process.env.TOKEN_KEY, async (err: any, data: any) => {
+    if (err) {
+      return res.json({ status: false });
+    } else {
+      const user = await User.findById(data.id);
+      if (user) return res.json({ status: true, user: user.username });
+      else return res.json({ status: false });
+    }
+  });
+};
