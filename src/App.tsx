bc00a733fc0df2d5499215aaab5b6916ab0ecba4
Yujin Cho <yujincho2002@gmail.com>
2024-04-22 17:51:46 -0700

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import Timer from './components/timer.tsx'
-import Left from './components/left.tsx'
-import Right from './components/right.tsx'
+import Timer from './components/Timer/timer.tsx'
 import List from './components/ToDoList/List.tsx';
 import './css/App.css';
 
 function App() {
   return (
     <div className = "App">
-      <div className="top-level">
+      <div className="flex justify-between flex-row h-screen w-[95vw]">
           <List/>
           <Timer />
-          <Right />
+          <List />
       </div>
     </div>
   );
