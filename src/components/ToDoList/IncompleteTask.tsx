9fbc06bbdb57dc482f7db845c04b25fec2e62d97
Justin Chang <chang.justin0106@gmail.com>
2024-05-01 20:46:22 -0700

    Changed theme

--- src/components/ToDoList/IncompleteTask.tsx
+++ src/components/ToDoList/IncompleteTask.tsx
@@ -1,6 +1,6 @@
 import Task from "./Task";
 import Unchecked from "../../assets/unchecked.svg"
-import BlackX from "../../assets/black_x.svg"
+import WhiteX from "../../assets/white_x.svg"
 
 type propType = {
     task: Task,
@@ -10,12 +10,12 @@ type propType = {
 
 export default function IncompleteTask(props: propType) {
     return (
-        <li className="flex flex-row list-none p-3.5 w-full items-center justify-between bg-white my-2 rounded-xl">
+        <li className="flex flex-row list-none p-3.5 w-full items-center justify-between bg-[#1D006F] my-2 rounded-xl">
             <div className="flex flex-auto items-center">
                 <img className="flex-initial mr-3 text-bold cursor-pointer" src={Unchecked} alt="Unchecked" onClick={() => props.onComplete(props.task)}/>
-                <span className="flex-initial text-black text-left overflow-wrap">{props.task.description}</span>
+                <span className="flex-initial text-black text-left text-white overflow-wrap">{props.task.description}</span>
             </div>
-            <img className="flex-end cursor-pointer" src={BlackX} alt="X" onClick={() => props.onDelete(props.task)}/>
+            <img className="flex-end cursor-pointer" src={WhiteX} alt="X" onClick={() => props.onDelete(props.task)}/>
         </li>
     );
 }
--- src/components/ToDoList/List.tsx
+++ src/components/ToDoList/List.tsx
@@ -43,18 +43,18 @@ export default function List() {
         <div className="relative flex flex-col h-[85%] bg-bgColor/10 
             rounded-[25px] self-center w-[22.5%] py-5 px-4 justify-between">
             <div className="flex flex-col">
-                <span className="my-3 self-start font-bold text-2xl">to-do list</span>
+                <span className="my-3 self-start font-bold text-4xl">to-do list</span>
                 <ul className="flex flex-col w-full">
                     {todo.map(element => <IncompleteTask task={element} onComplete={onComplete} onDelete={deleteIncomplete}/>)}
                     {finished.map(element => <CompleteTask task={element} onIncomplete={onIncomplete} onDelete={deleteComplete}/>)}
                 </ul>
             </div>
             <label className="flex flex-row items-center justify-between">
-                <input className="flex-grow pl-3 mr-1 bg-slate-200 rounded-[25px] text-black placeholder-gray-500 h-full" 
+                <input className="flex-grow pl-3 mr-1 bg-slate-200 rounded-full bg-[#1D006F] placeholder-white h-full" 
                     placeholder="add items here..." value={addText} 
                     onChange={e => setAddText(e.target.value)}
                     onKeyDown={handleKeyEvent}/>
-                <button className={`rounded-full text-2xl font-bold items-center 
+                <button className={`rounded-full text-2xl text-black font-bold items-center 
                     py-2 px-4 h-full cursor-pointer ${addText.length == 0 && "bg-gray-400"}`}
                     disabled={addText.length == 0} 
                     onClick={addTask}>+</button>
