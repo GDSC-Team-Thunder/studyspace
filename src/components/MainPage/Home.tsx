e7b5d46ba39fc51b3ecdfaec19aba1ed5efa28f9
Justin Chang <chang.justin0106@gmail.com>
2024-05-27 18:39:49 -0700

    Added routing for Home page

--- src/components/MainPage/Home.tsx
+++ src/components/MainPage/Home.tsx
@@ -1,15 +1,18 @@
 import Timer from "../Timer/timer.tsx";
 import List from "../ToDoList/List.tsx";
 import Right from "../right.tsx";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-center items-center flex-col h-screen w-screen">
       <div className="flex flex-row justify-between items-center mt-5 w-[95vw]">
         <p className="flex-grow-0 text-[48px] font-bold">studyspace</p>
         <div className="flex flex-row ml-auto space-x-4">
-          <p className="flex flex-grow-0 text-[24px] font-bold">sign up</p>
-          <p className="flex flex-grow-0 text-[24px] font-bold">log in</p>
+          <p className="flex flex-grow-0 text-[24px] font-bold cursor-pointer" onClick={() => navigate("/Signup")}>sign up</p>
+          <p className="flex flex-grow-0 text-[24px] font-bold cursor-pointer" onClick={() => navigate("/login")}>log in</p>
         </div>
       </div>
       <div className="flex flex-grow justify-between flex-row w-[95vw]">
